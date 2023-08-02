0c33f27393eeeb53dbf1cf050c7ba2d7a2c08c5f
bareldan <bareldan@fit.cvut.cz>
2023-08-02 19:51:45 +0600

    final version with gitignore

---
+++ bareldan/src/main.cpp
@@ -0,0 +1,20 @@
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "../include/client_message.h"
+#include "../include/number.h"
+#include "../include/table.h"
+#include "../include/text.h"
+#include "../libs/json.hpp"
+#include "../libs/tabulate.hpp"
+
+int main(void) {
+  ClientMessage message;
+  message.getCommand();
+
+  return 0;
+}
